8e15ea97a55f87b340b95fc776fd3d5f94d51e6e
Eli Wemyss <eliwemyss@gmail.com>
2019-04-11 11:40:11 -0700

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
+import './layout.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -31,10 +24,10 @@ const Layout = ({ children }) => (
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            paddingTop: 0
           }}
         >
-          <main>{children}</main>
+          {children}
           <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
@@ -44,10 +37,10 @@ const Layout = ({ children }) => (
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,65 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import Image from 'gatsby-image';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-const IndexPage = () => (
+export const query = graphql`
+  {
+    allSanityProject {
+      edges {
+        node {
+          title
+          description
+          slug {
+            current
+          }
+          image {
+            asset {
+              fluid {
+                ...GatsbySanityImageFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>My Portfolio</h1>
+    <ul
+      style={{
+        listStyle: 'none',
+        display: 'flex',
+        alignItems: 'space-between',
+        padding: 0
+      }}
+    >
+      {data.allSanityProject.edges.map(({ node: project }) => (
+        <li
+          key={project.slug.current}
+          style={{
+            flex: '1 45%',
+            flexWrap: 'wrap',
+            maxWidth: '45%',
+            margin: '1rem'
+          }}
+        >
+          <h2 style={{ fontSize: '24px' }}>
+            <Link to={project.slug.current}>{project.title}</Link>
+          </h2>
+          <Image fluid={project.image.asset.fluid} alt={project.title} />
+          <p style={{ marginTop: '1rem' }}>{project.description}</p>
+          <Link to={project.slug.current}>See project details</Link>
+        </li>
+      ))}
+    </ul>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
